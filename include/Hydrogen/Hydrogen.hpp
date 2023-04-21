d5611c8d5f173b70043f3b68e44375c2ded68f92
Nico Grundei <malediktusrgb@gmail.com>
2023-04-21 22:20:42 +0200

    Fixed hydrogen header

--- include/Hydrogen/Hydrogen.hpp
+++ include/Hydrogen/Hydrogen.hpp
@@ -1,15 +1,1 @@
 #pragma once
-
-#include "Buffer.hpp"
-#include "Camera.hpp"
-#include "Context.hpp"
-#include "Core.hpp"
-#include "RenderCommand.hpp"
-#include "Renderer.hpp"
-#include "RendererAPI.hpp"
-#include "Shader.hpp"
-#include "Texture.hpp"
-#include "VertexArray.hpp"
-#include "Framebuffer.hpp"
-#include "Renderbuffer.hpp"
-#include "Scene/Scene.hpp"
