34ff51c921c5079b7e431ce7782b0ba9a63f6786
Nico Grundei <malediktusrgb@gmail.com>
2023-04-21 23:31:38 +0200

    Added shared_ptr abstraction

--- include/Hydrogen/Core/Memory.hpp
+++ include/Hydrogen/Core/Memory.hpp
@@ -1,1 +1,12 @@
 #pragma once
+
+#include <memory>
+
+namespace Hydrogen {
+template <typename T> using Reference = std::shared_ptr<T>;
+template <typename T> using Reference = std::shared_ptr<T>;
+
+template <typename T, typename... Args> constexpr Reference<T> NewReference(Args&&... args) {
+    return std::make_shared<T>(std::forward<Args>(args)...);
+}
+} // namespace Hydrogen
